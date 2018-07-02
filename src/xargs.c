3b1db01b2756a3bce458f080da0b300d4a071888
Brian Lindsay <blindsay@iastate.edu>
2018-07-02 18:08:19 -0500

    Corrected char** passed to execvp and added extrenuous commandline args to execvp

--- src/xargs.c
+++ src/xargs.c
@@ -1,3 +1,12 @@
+/*
+Executes the command passed as commandline arguments with the data recieved
+via stdin/pipe.
+
+TODO:
+  Write a better split_line.
+  Write a char** append function.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +14,10 @@
 
 #include <sys/wait.h>
 
+/*
+Splits the given line by spaces and places them into args.
+arg_size is the number of splits
+*/
 int split_line(const char* line, char **args, int *arg_size){
   int end = 0;
   int start = 0;
@@ -21,10 +34,6 @@ int split_line(const char* line, char **args, int *arg_size){
       args[*arg_size] = (char *) malloc(sizeof(char) * tmp_len + 1);
       memcpy(args[*arg_size], (line + start), tmp_len);
       args[*arg_size][tmp_len] = '\0';
-      if(!strcmp(";", args[*arg_size])){
-		free(args[*arg_size]);
-		args[*arg_size] = NULL;
-      }
       start = end;
       (*arg_size)++;
     }
@@ -33,28 +42,43 @@ int split_line(const char* line, char **args, int *arg_size){
 }
 
 int main(int argc, char **argv){
-	char *total;
-	total = calloc(1024, sizeof(char));
-	
-	char *buffer;
-	buffer = calloc(1024, sizeof(char));
-	size_t bufsize = 1024;
-	int len;
-	while((len = getline(&buffer, &bufsize, stdin)) > 0){
-		buffer[len - 1] = '\0';
-		strcat(total, buffer);
-		strcat(total, " ");
-	}
-	printf("%s\n", total);
-	char **args = (char **) calloc(64, sizeof(char *));
-	int c = 0;
-	split_line(total, args, &c);
-	
-	pid_t pid = fork();	 
-	if(pid == -1){
-	}else if(!pid){
-	  execvp(argv[1], args);
-	}else{
-		wait(NULL);
-	}
+  char *total;
+  total = calloc(1024, sizeof(char));
+  
+  char *buffer;
+  buffer = calloc(1024, sizeof(char));
+  size_t bufsize = 1024;
+  int len;
+  while((len = getline(&buffer, &bufsize, stdin)) > 0){
+    strcat(total, buffer);
+  }
+  char **args = (char **) calloc(64, sizeof(char *));
+  int c = 0;
+  split_line(total, args, &c);
+  int num_args = c + argc - 1;
+  int pass_args = argc - 1;
+  while(num_args >= pass_args){
+    args[num_args] = args[num_args - pass_args];
+    num_args--;
+  }
+  
+  while(num_args >= 0){
+    args[num_args] = (char *) calloc(strlen(argv[num_args + pass_args]) + 1, sizeof(char));
+    strcpy(args[num_args], argv[num_args + pass_args]);
+    num_args--;
+  }
+  pid_t pid = fork();	 
+  if(pid == -1){
+  }else if(!pid){
+    execvp(argv[1], args);
+  }else{
+    wait(NULL);
+  }
+  char **ptr = args;
+  while(*ptr)
+    free(*(ptr++));
+  free(args);
+    
+  free(buffer);
+  free(total);
 }
