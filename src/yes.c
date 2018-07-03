83ac17dfe7d19880e66724764a97c9f7a006e6d9
Brian Lindsay <blindsay@iastate.edu>
2018-07-03 16:40:45 -0500

    Removed else and reduced the while(1) to while(puts())

--- src/yes.c
+++ src/yes.c
@@ -1,25 +1,26 @@
 /*
 yes
 Repeatedly outputs provided text or "y" until killed.
+
+AUTHOR: Micky Lindsay
+CREATED: 7/2/2018
+
+TODO: Think of a way to remove the for loop
 */
 
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char **argv){
-	int i;
-	char *buf;
-	for (i = 1; i < argc - 1; i++){
-		argv[i][strlen(argv[i])] = ' ';
-	}
-	
-	if(argc == 1)
-		buf = "y";	
-	else
-		buf = *(argv+1);
-	
-	while(1){
-		puts(buf);
-	}
-	return 1;
+  int i;
+  char *buf = "y";
+  //Kind of a hack. Since argv is continuous,
+  //change the null termination to a space.
+  for (i = 1; i < argc - 1; i++){
+    argv[i][strlen(argv[i])] = ' ';
+  }
+  
+  if(i > 1) buf = *(argv+1);
+  while(puts(buf) > 0);
+  return 1;
 }
