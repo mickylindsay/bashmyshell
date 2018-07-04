14b9eddc5bee9957cacd1e62a8ae9faeacf675e1
Brian Lindsay <blindsay@iastate.edu>
2018-07-04 18:31:20 -0500

    added H ANSI Escape Code

--- src/clear.c
+++ src/clear.c
@@ -17,6 +17,7 @@
 Clears the current terminal and exits
 puts declaration instead of header include to reduce executable size
 2J is the ANSI escape code for clear screen and return cursor
+H is the ANSI escape code to reset cursor to top left
 */
 
 #define PACKAGE_NAME "bashmyshell"
@@ -27,4 +28,5 @@ puts declaration instead of header include to reduce executable size
 int puts(const char *);
 int main(void){
   puts("\x1b[2J");
+  puts("\x1b[H");
 }
