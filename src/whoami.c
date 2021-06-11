81ea2d59ced202423ada69ed007bee972ade8b83
Micky Lindsay <mic@yousq.net>
2021-06-10 23:07:39 -0700

    whoami v0.2

--- src/whoami.c
+++ src/whoami.c
@@ -19,18 +19,59 @@ Prints the effective user
 
 #include <stdio.h>
 #include <unistd.h>
-#include <pwd.h>
 
 #define PACKAGE_NAME "bashmyshell"
 #define PROGRAM_NAME "whoami"
-#define VERSION "0.1"
+#define VERSION "0.2"
 #define AUTHORS "Micky Lindsay"
+
+// v0.2 No longer uses geteuid or getpwuid.
+// Instead reads /etc/passwd file, and make SYS call for euid
+// Efficient? No.
+// And more, much more than this I did it, my way
+
+int main(void){
+  char buffer[256];
+  char id[32];
+
+  sprintf(id, "%ld", syscall(107));
+
+  FILE *passwd = fopen("/etc/passwd", "r");
+  while (fgets(buffer, sizeof(buffer), passwd)){
+    char *i = buffer;
+    char *j = id;
+    int num_colon = 0;
+    // Step through passwd file line until past second colon ':'
+    while (num_colon < 2)
+      if ((i++)[0] == ':') num_colon++;
+
+    // Walk through effective uid and uid in passwd file
+    while (j[0] == i[0]) {
+      j++;
+      i++;
+    }
+
+    // If passwd file has reach a colon AND uid string is null byte,
+    // found effective user line
+    if (j[0] == 0 && i[0] == ':') break;
+  }
+
+  // Print current user character by character,
+  // because I cant be fucked writting a clever substring
+  char *c = buffer;
+  while (c[0] != ':') {
+    printf("%c", (c++)[0]);
+  }
+  printf("\n");
+  // puts(getpwuid(id)->pw_name);
+  return 0;
+}
 
 // v0.1 Uses both unistd.h and pwd.h. :puke:
 // Next step remove getpwuid call.
 
-int main(void){
-  uid_t id = geteuid();
-  printf("%s\n", getpwuid(id)->pw_name);
-  return 0;
-}
+// int main(void){
+//   uid_t id = geteuid();
+//   printf("%s\n", getpwuid(id)->pw_name);
+//   return 0;
+// }
